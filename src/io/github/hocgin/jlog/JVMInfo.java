86378a2baddb7831590c2ee8356935407242dbb4
hocgin <hocgin@gmail.com>
2016-04-22 10:45:21 +0800

    update

--- src/io/github/hocgin/jlog/JVMInfo.java
+++
@@ -1,7 +0,0 @@
-package io.github.hocgin.jlog;
-
-/**
- * Created by moy on 16-1-11.
- */
-public class JVMInfo {
-}
--- test/Main.java
+++ test/Main.java
@@ -1,31 +1,48 @@
-import com.google.gson.Gson;
-import io.github.hocgin.jlog.FormatSingle;
+import in.hocg.jlog.JLog;
+import in.hocg.jlog.enums.Foreground;
+import in.hocg.jlog.util.JRender;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Created by ubuntu on 16-1-7.
- *
- / JLog Class Method
-$-------------------
- 内存/Jdk/OS
-
-
+ * Created by hocgin on 16-4-21.
  */
 public class Main {
-    public static void main(String args[]) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException {
-        Gson gson = new Gson();
-//        String s = gson.toJson(new TestBean("n", "val"));
-//        System.out.println("-->" + Colors.B_Black.getCode() + Colors.F_Bed.getCode() + Colors.Border.getCode() + s + Colors.None.getCode());
+
+    public static void main(String[] args) {
+        JLog.init().setMethodCount(2);
 
 
-        Class<?> aClass = ClassLoader.getSystemClassLoader().loadClass("com.google.gson.Gson");
-        Method toJson = aClass.getDeclaredMethod("toJson", Object.class);
-        Object ret = toJson.invoke(aClass.newInstance(), new TestBean("n", "val"));
-        String s = FormatSingle.NEW().toJson(new TestBean("n", "val"));
-        System.out.println("  # --> " + s);
+        List<User> users = new ArrayList<>();
+        User user;
+        for (int i = 0; i < 2; ) {
+            user = new User("No." + ++i);
+            users.add(user);
+        }
+        User xiaoMing = new User(users, "xiaoMing");
 
+        JLog.json(users);
+        JLog.json(xiaoMing);
+        JLog.d("d Hello World");
+        JLog.v("v Hello World");
+        JLog.e("e Hello World");
+        JLog.i("i Hello World");
+        JLog.w("w Hello World");
+        JLog.wtf("wtf Hello World");
+        JLog.e(new IndexOutOfBoundsException(), "e xnull");
+        JLog.xml("<is>xml</is>");
+        JLog.tag("this is tag").xml("<is>xml</is>");
 
+        System.out.println(JRender.color("Azure &&&", Foreground.Azure));
+        System.out.println(JRender.color("Purple &&&", Foreground.Purple));
+        System.out.println(JRender.color("Blue &&&", Foreground.Blue));
+        System.out.println(JRender.color("Black &&&", Foreground.Black));
+        System.out.println(JRender.color("Green &&&", Foreground.Green));
+        System.out.println(JRender.color("Red &&&", Foreground.Red));
+        System.out.println(JRender.color("White &&&", Foreground.White));
+        System.out.println(JRender.color("Yellow &&&", Foreground.Yellow));
     }
+
+
 }
